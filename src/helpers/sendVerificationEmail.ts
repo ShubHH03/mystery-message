4d6fbdc3fb68fc3694d461a1e6a22441c7023e7f
ShubHH03 <mak.shubham2003@gmail.com>
2025-09-01 17:34:32 +0530

    Add verification email functionality and integrate resend service

---
+++ src/helpers/sendVerificationEmail.ts
@@ -0,0 +1,6 @@
+import { resend } from "@/lib/resend";
+import VerificationEmail from "../../emails/VerificationEmail";
+import { ApiResponse } from "@/types/ApiResponse";
+
+export async function sendVerificationEmail(
+){}

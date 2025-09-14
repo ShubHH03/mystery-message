12b7cab08b69ee15d285dacedaacc80de9aa9908
ShubHH03 <mak.shubham2003@gmail.com>
2025-09-14 18:42:43 +0530

    add Separator and Switch components using Shadcn UI

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+"use client";
+
+import { Message } from "@/model/User";
+import { acceptMessageSchema } from "@/schema/acceptMessageSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useSession } from "next-auth/react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import axios from "axios";
 
 const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSwitchLoading, setIsSwitchLoading] = useState(false)
 
-export default page
+  const handleDeleteMessage = (messageId: string) => {
+   setMessages(messages.filter((message) => message._id !== messageId)) 
+  }
+
+  const {data: session} = useSession()
+
+  const form = useForm({
+    resolver: zodResolver(acceptMessageSchema)
+  })
+
+  const {register, watch, setValue} = form;
+
+  const acceptMessages = watch('acceptMessages')
+
+  return <div>page</div>;
+};
+
+export default page;

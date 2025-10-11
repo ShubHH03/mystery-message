c388a41912db439c3829e420de81425f2677d98b
ShubHH03 <mak.shubham2003@gmail.com>
2025-10-11 16:43:57 +0530

    remove Navbar component from RootLayout to streamline layout structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
-import Navbar from "@/components/Navbar";
 import AuthProvider from "@/context/AuthProvider";
 
 const geistSans = Geist({
@@ -30,7 +29,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <AuthProvider>
-          <Navbar />
           {children}
           <Toaster richColors/>
         </AuthProvider>

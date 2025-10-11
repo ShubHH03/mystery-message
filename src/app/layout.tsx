84b5d0c18a3139845451dc6c350b6bed94bfa906
ShubHH03 <mak.shubham2003@gmail.com>
2025-10-11 16:40:31 +0530

    uncomment Navbar component in RootLayout for proper navigation display

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <AuthProvider>
-          {/* <Navbar /> */}
+          <Navbar />
           {children}
           <Toaster richColors/>
         </AuthProvider>
